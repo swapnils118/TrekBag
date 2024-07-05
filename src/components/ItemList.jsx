6df3dcc3bc421dfc56fea67abba85f95ed8a99ca
swapnils118 <swapnil118@icloud.com>
2024-07-05 02:25:40 -0400

    Sorting panel functionality added

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -8,6 +8,7 @@
 
 import Select from "react-select";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
 
 const sortingOptions = [
   {
@@ -29,17 +30,34 @@ export default function ItemList({
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+
+    return;
+  });
+
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView /> : null}
 
       {items.length > 0 ? (
         <section className="sorting">
-          <Select defaultValue={sortingOptions[0]} options={sortingOptions} />
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
         </section>
       ) : null}
 
-      {items.map((item) => {
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.id}
